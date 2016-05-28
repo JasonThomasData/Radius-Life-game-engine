bbb45ea50fcb8ce66c9988ecdf3cfe8c74536d78
JasonThomasData <jason.robert.thomas.123@gmail.com>
2016-05-29 02:15:59 +1000

    Further abstractions out of main js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+//The loop that loops through all players, then loops through all of each player's units, and moves them
+//Each unit gets to move a pixel, or whatever the speed is set to, if the unit belongs to the player whose turn it is currently.
+var logic_loop = setInterval(function(){
+    var i = game_settings.turn
+    obstacles_collection.remove_destroyed()
+    player_remove_dead(players_all[i])
+    if (players_all[i].units.length > 0){
+        player_units_move(players_all[i])
+    } else {
+        game_settings.turn ++
+        if (i >= players_all.length - 1){
+            game_settings.turn = 0
+        } 
+    }
+}, game_settings.between_loops)
+
+
 //Initialises canvas
 var canvas = document.getElementById("canvas");
 canvas.width = game_settings.canvas.width //These sizes will be overwritten when the file is uploaded
@@ -8,12 +25,15 @@ var ctx = canvas.getContext("2d");
 function draw(){
     ctx.clearRect(0, 0, canvas.width, canvas.height); //clears canvas
     //Place all units
-    for (var i = 0; i < players.length; i++){
-        for (var j = 0; j < players[i].units.length; j++){
-            if (players[i].units[j].radius > 0){
+    for (var i = 0; i < players_all.length; i++){
+        for (var j = 0; j < players_all[i].units.length; j++){
+            if (players_all[i].units[j].radius > 0){
                 ctx.beginPath();
-                ctx.arc(players[i].units[j].current_pos[0] + game_settings.units.spaces_per_move / 2, players[i].units[j].current_pos[1] + game_settings.units.spaces_per_move / 2, players[i].units[j].radius + players[i].units[j].min_radius, 0, 2 * Math.PI)
-                ctx.fillStyle = players[i].colour
+                var player_x = players_all[i].units[j].current_pos[0] + game_settings.units.spaces_per_move / 2
+                var player_y = players_all[i].units[j].current_pos[1] + game_settings.units.spaces_per_move / 2
+                var player_radius = players_all[i].units[j].radius + players_all[i].units[j].min_radius
+                ctx.arc(player_x, player_y, player_radius, 0, 2 * Math.PI)
+                ctx.fillStyle = players_all[i].colour
                 ctx.fill()                
             }
         }
@@ -38,44 +58,3 @@ function draw_loop(timestamp) {
     requestAnimationFrame(draw_loop);
 }
 requestAnimationFrame(draw_loop);
-
-//The loop that loops through all players, then loops through all of each player's units, and moves them
-//Each unit gets to move a pixel, or whatever the speed is set to, if the unit belongs to the player whose turn it is currently.
-var logic_loop = setInterval(function(){
-    var i = game_settings.turn
-    obstacles_collection.remove_destroyed()
-    players[i].remove_dead()
-    if (players[i].units.length > 0){
-        players[i].units_move()
-    } else {
-        game_settings.turn ++
-        if (i >= players.length - 1){
-            game_settings.turn = 0
-        } 
-    }
-}, game_settings.between_loops)
-
-//Below, everythin that manages players
-
-var players = []
-
-function randomise_players(){
-    var temp_players = []
-    while (players.length > 0){
-        var i = Math.floor(Math.random() * (players.length))
-        temp_players.push(players[i])
-        players.splice(i, 1)
-    }
-    players = temp_players    
-}
-
-//TODO - place this inside the map generator. If a g is detected, generator green_player
-
-var green_player = new player_object('#00cc00', 'standard')
-var yellow_player = new player_object('#ffcc00', 'standard')
-var blue_player = new player_object('#0066cc', 'standard')
-var red_player = new player_object('#e62e00', 'standard')
-var human_player = new player_object('#336699', 'player')
-var zombie_player = new player_object('#669900', 'standard')
-
-randomise_players()
