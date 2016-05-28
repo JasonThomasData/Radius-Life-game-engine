16602350e48a35330365a7bfefb2638876360d35
JasonThomasData <jason.robert.thomas.123@gmail.com>
2016-05-29 02:19:15 +1000

    Removed player actions from player object, separating concerns

---
+++ js/player_actions.js
@@ -0,0 +1,50 @@
+function player_create_new_unit(player, coords){
+    if(player.unit_type == 'standard'){
+        var new_unit = new unit(player.colour, coords);            
+    } else if (player.unit_type == 'player'){
+        var new_unit = new human_unit(player.colour, coords);
+    }
+    player.units.push(new_unit)
+}
+
+function player_units_move(player){
+    for (var i = 0; i < player.units.length; i++){
+        if (player.units[i].turn_going == true){
+            unit_take_turn(player.units[i])
+        }
+    }
+    player_check_moves(player)
+}
+
+function player_remove_dead(player){
+    for (var i = 0; i < player.units.length; i++){
+        if (player.units[i].hit_points <= 0){
+            console.log(player.units[i].current_pos, ' died')
+            player.units.splice(i,1)
+        }
+    }
+}
+
+//Every unit gets one action each. Each unit has a callback for when an action is complete. That calls this function, and adds one to the tally. If the tally equals the number of units, then that player's units have all moved. The logic loop will now target the next player
+function player_reset_units(player){
+    for (var i = 0; i < player.units.length; i++){
+        player.units[i].turn_going = true
+    }
+}
+
+function player_check_moves(player){
+    var going = 0
+    for (var i = 0; i < player.units.length; i++){
+        if (player.units[i].turn_going == true){
+            going ++
+        }
+    }
+    if (going == 0){
+        player_reset_units(player)
+        if (game_settings.turn == players_all.length -1){
+            game_settings.turn = 0
+        } else {
+            game_settings.turn ++
+        }
+    }
+}
